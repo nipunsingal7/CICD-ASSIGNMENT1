0c5bd1cfbb2dd36ede7932e216f7e951ca0414f2
Nipun Singal <nipunsingal7@gmail.com>
2020-11-18 22:08:13 +0530

    first commit

---
+++ CICDass2/src/main/java/cicd/Class1.java
@@ -0,0 +1,16 @@
+package cicd;
+
+
+public class Class1 {
+	
+	int sum(int a,int b)
+	{
+		return(a+b);
+	}
+	
+public static void main(String args[])
+{
+	Class1 obj1 =new Class1();
+	System.out.println(obj1.sum(8,5));
+}
+}
